f1a7b4a062ef4f36e02bbaad7e317b2b231aadd3
Tatiana <tttatttu@gmail.com>
2022-02-13 12:46:19 +0300

    правки ПР

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const router = express.Router()
 const app = express();
 const PORT = 3000;
 
@@ -12,29 +11,3 @@ app.listen(PORT, function () {
 
 app.use(express.static(staticPath));
 
-app.get('./', (req, res) => {
-  res.sendFile( staticPath,'index.hbs');
-});
-
-router.get('./authorization', (req, res) => {
-  res.sendFile(staticPath, 'authorization.hbs');
-});
-
-router.get('/chat', (req, res) => {
-  res.sendFile(path.join(staticPath, 'chat.hbs'));
-});
-
-router.get('/profile', (req, res) => {
-  res.sendFile(path.join(staticPath, 'profile.hbs'));
-});
-
-router.get('/404', (req, res) => {
-  res.sendFile(path.join(staticPath, '404.hbs'));
-});
-
-
-router.get('/500', function (req, res) {
-  res.sendFile(path.join(staticPath, '500.hbs'));
-})
-
-module.exports = router;
