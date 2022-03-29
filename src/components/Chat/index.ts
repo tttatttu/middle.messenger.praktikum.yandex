e5f32d53f0c4359ca73ecc08ab0150a13466ebc6
Tatiana <tttatttu@gmail.com>
2022-03-29 12:37:43 +0300

    добавлены сообщения при клике на чат

---
+++ src/components/Chat/index.ts
@@ -0,0 +1,7 @@
+// import { ChatList } from './chat';
+// import { withChats } from '../../utils/Store';
+//
+// // @ts-ignore
+// export default withChats(ChatList);
+
+export { ChatList as default } from './chatList';
